c2712ba5b0774acf24c075b22dd247c61c250764
Rune <runal_1998@hotmail.com>
2019-10-13 00:58:09 +0200

    no progress just updates

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -18,13 +19,24 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,11 +49,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         final EditText editText = findViewById(R.id.editText); //The edit field of the application
-        final Geocoder coder = new Geocoder(getApplicationContext());
-        final List<Address> address;
+        final TextView temperatureText = findViewById(R.id.textView); // text field that displays temperature
 
+        final double[] latandlng = new double[2];  //(latitude, longitude)
 
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -50,12 +61,24 @@ public class MainActivity extends AppCompatActivity {
                         (actionId == EditorInfo.IME_ACTION_DONE)){
                     LatLng location = getLocationFromAddress(getApplicationContext(), editText.getText().toString());
                     if(location != null){
-                        editText.setText("lat: "+location.latitude + " lon: "+ location.longitude);
+                        latandlng[0] = location.latitude;
+                        latandlng[1] = location.longitude;
+                    }else{
+                        // Default Bergen
+                        location = getLocationFromAddress(getApplicationContext(), "Bergen");
+                        latandlng[0] = location.latitude;
+                        latandlng[1] = location.longitude;
                     }
+
+
+                    String url = String.format("https://api.met.no/weatherapi/locationforecast/1.9/.json?lat=%f&lon=%f&msl=%d", latandlng[0], latandlng[1], 1);
+                    
                 }
                 return false;
             }
         });
+
+
     }
 
     public LatLng getLocationFromAddress(Context context,String strAddress) {
